c1c73ff170234f626b4714cb2d547644e2002b86
Pawel Burakowski <pawel.burci@wp.pl>
2022-12-10 19:31:18 +0100

    Added login.php feature

---
+++ login.php
@@ -0,0 +1,77 @@
+<?php
+
+session_start();
+
+require_once "connect.php";
+
+if ((!isset($_POST['login'])) || (!isset($_POST['password'])))
+	{
+		header('Location: index.php');
+		exit();
+	}
+
+	try
+	{
+        $connection = new mysqli($host, $db_user, $db_password, $db_name);
+		
+	    if ($connection->connect_errno!=0)
+        {
+            throw new Exception(mysqli_connect_errno());
+        }
+
+        else 
+        {
+            $login = $_POST['login'];
+            $password = $_POST['password'];
+
+            $login = htmlentities($login, ENT_QUOTES, "UTF-8");
+
+            if ($result = $connection->query(sprintf("SELECT * FROM users WHERE username='%s'",mysqli_real_escape_string($connection,$login))))
+            {			
+                $usersNumber= $result->num_rows;
+                if($usersNumber > 0)
+                {
+                    $row = $result->fetch_assoc();
+
+                    if (password_verify($password, $row['password']))
+                    {
+                        $_SESSION['isLogin'] = true;
+                        $_SESSION['userId'] = $row['id'];
+                        $_SESSION['username'] = $row['username'];
+                        
+                        unset($_SESSION['error']);
+                        $result->free_result();
+                        header('Location: mainmenu.php');
+                    }
+
+                    else
+                    {
+                        $_SESSION['error'] = '<span style="color:red">Nieprawidłowy login lub hasło!</span>';
+                        header('Location: index.php');
+                    }
+                }
+
+                else 
+                {
+                    $_SESSION['error'] = '<span style="color:red">Nieprawidłowy login lub hasło!</span>';
+                    header('Location: index.php');
+                }
+            }
+
+            else
+            {
+                throw new Exception($connection->error);
+                header('Location: index.php');
+            }	
+        }
+
+        $connection->close();
+       }
+        
+	catch(Exception $e)
+		{
+			echo '<span style="color:red;">Błąd serwera! Spróbuj ponownie później!</span>';
+			echo '<br />Informacja developerska: error 404 :( '.$e;
+		}
+
+?>
